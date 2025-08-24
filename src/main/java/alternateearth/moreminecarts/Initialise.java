c6fede9dd378baaec5f41d03e28b5f3e61d7a93d
Kyle Laverty <k.laverty@gmail.com>
2025-08-23 22:23:51 -0400

    Setup basic structure of mod repo

---
+++ src/main/java/alternateearth/moreminecarts/Initialise.java
@@ -0,0 +1,16 @@
+package alternateearth.moreminecarts;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class Initialise implements ModInitializer {
+
+    public static final String MOD_ID = "moreminecarts";
+    public static final String MOD_NAME = "More Minecarts";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+    }
+}
